1a62738f7aadd75c2738dccd6f8390f339ee88b4
Adonai Cruz <adonai.cruz@gmail.com>
2017-01-27 12:45:58 -0200

    initial html content

---
+++ assets/scripts/main.js
@@ -0,0 +1,19 @@
+$(function(){
+	// Creates the element for the alert
+	var loading = $('<span class="loading"></span>');
+	loading.insertAfter($('#persist'));
+
+	$('#persist').click(function(){
+		var me = $(this);
+		loading.html('Aguarde, buscando dados...');
+		loading.removeClass('error sucess');
+		$.get( "persist.php", function(data) {
+		  loading.addClass('sucess');
+		  loading.html(data.message);
+		},'json')
+		.fail(function(data) {
+		  loading.addClass('error');
+		  loading.html(data.message);
+		});
+	});
+});
